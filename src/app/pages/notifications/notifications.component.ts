364c33c2f71d02b9db044ac4120897a4aad87252
Ganesh Kusumbe <ganeshkusumbe@Ganeshs-MacBook-Air.local>
2025-04-04 08:49:45 +0530

    added code

--- src/app/pages/notifications/notifications.component.ts
+++ src/app/pages/notifications/notifications.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-
+import { IonApp, IonContent } from "@ionic/angular/standalone";
+import { IonicModule } from '@ionic/angular';
 @Component({
   selector: 'app-notifications',
   templateUrl: './notifications.component.html',
   styleUrls: ['./notifications.component.scss'],
+  imports: [IonicModule]
 })
 export class NotificationsComponent  implements OnInit {
 
