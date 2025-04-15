5711d24192dd7a973644fca304aa03b4a4643cd7
Ganesh Kusumbe <ganeshkusumbe@Ganeshs-MacBook-Air.local>
2025-04-15 21:09:41 +0530

    added new change in footer tab

--- src/app/shared/footer/footer.page.ts
+++ src/app/shared/footer/footer.page.ts
@@ -1,25 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import {  IonToolbar, IonFooter, IonIcon, IonButton, IonTabButton, IonLabel, IonBadge } from '@ionic/angular/standalone';
+import {  IonToolbar, IonFooter, IonIcon, IonButton, IonTabButton, IonLabel, IonBadge, IonTab, IonTabs, IonTabBar } from '@ionic/angular/standalone';
 import { MatIconModule } from '@angular/material/icon'
 import { RouterLink } from '@angular/router';
 import { addIcons } from 'ionicons';
-import { home , chatbubble   ,  notificationsSharp , chatboxSharp, homeSharp, personSharp} from 'ionicons/icons'; // ✅ Import the icon(s) you want to use
+import { home , chatbubble   ,  notificationsSharp , chatboxSharp, homeSharp, personSharp, people, card, storefront, settings } from 'ionicons/icons'; // ✅ Import the icon(s) you want to use
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.page.html',
   styleUrls: ['./footer.page.scss'],
   standalone: true,
-  imports: [  IonIcon , IonButton,  IonFooter, MatIconModule ,RouterLink  ,IonToolbar, CommonModule, FormsModule]
+  imports: [IonTabBar, IonTabs, IonTabButton , IonLabel,  IonIcon , MatIconModule ,RouterLink  , CommonModule, FormsModule]
 })
 export class FooterPage implements OnInit {
 
   constructor() { 
-  addIcons({
-  home , personSharp , chatbubble ,notificationsSharp , chatboxSharp , homeSharp
-  })
+  addIcons({people,card,storefront,settings,homeSharp,notificationsSharp,chatboxSharp,personSharp,home,chatbubble});
 }
   ngOnInit() {
   }
