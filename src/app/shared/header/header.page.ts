f2abe367653787a34042168706e7d784bebd71ae
Ganesh Kusumbe <ganeshkusumbe@Ganeshs-MacBook-Air.local>
2025-05-02 01:09:19 +0530

    added new changes

--- src/app/shared/header/header.page.ts
+++ src/app/shared/header/header.page.ts
@@ -2,22 +2,23 @@ import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonMenuButton, IonTitle, IonToolbar, IonTab, IonButtons, IonIcon } from '@ionic/angular/standalone';
-import { menuSharp } from 'ionicons/icons';
+import { menuSharp, notificationsSharp } from 'ionicons/icons';
 import { addIcons } from 'ionicons';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.page.html',
   styleUrls: ['./header.page.scss'],
   standalone: true,
-  imports: [IonMenuButton , IonHeader, IonButtons, IonTitle , IonToolbar , CommonModule, FormsModule]
+  imports: [IonMenuButton ,RouterLink, IonIcon, IonHeader, IonButtons, IonTitle , IonToolbar , CommonModule, FormsModule]
 })
 export class HeaderPage implements OnInit {
 
   @Input() pageName: string = "";
   
   constructor() { 
-    addIcons({ menuSharp });
+    addIcons({notificationsSharp,menuSharp});
   }
 
   ngOnInit() {
